971e282b943abe3455de96672273d9b6f208b385
asaybel <pyjioh@gmail.com>
2016-01-19 17:51:13 +0500

    Add panels 'writing'

--- js/index.js
+++ js/index.js
@@ -2,5 +2,8 @@
  * Created by asaybel on 18.01.2016.
  */
 $(document).ready(function () {
-
+    $('.cell').click(function () {
+        var color = $(this).css('background-color') == 'rgb(0, 0, 0)' ? 'white' : 'black';
+        $(this).css('background-color', color);
+    });
 });
