ce732f13083ebf679a3c19a392ab6d6e98726022
pyjioh <pyjioh@gmail.com>
2016-01-21 00:51:29 +0500

    Add training data json file

--- js/index.js
+++ js/index.js
@@ -14,7 +14,15 @@ $(document).ready(function () {
             var i = $(this).css('background-color') == 'rgb(0, 0, 0)' ? 1 : 0;
             data.push(i);
         });
+        var data = $.getJSON("data/data.json", function (data) {
+            var items = [];
+            $.each(data, function (key, val) {
+                console.log(key + ' ' + val);
+            });
+        });
         var network = new Network();
+        network.train();
+
     });
 });
 
