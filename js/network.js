e3492585a4b268b8e87f069a04d7d12f0d71d00d
asaybel <pyjioh@gmail.com>
2016-01-21 19:03:46 +0500

    Export required functions

--- js/network.js
+++ js/network.js
@@ -1,32 +1,39 @@
 /**
  * Created by asaybel on 20.01.2016.
  */
+var netjs = netjs || {};
 
-var Network = function (options) {
-    var options = options || {};
+(function (global) {
+    'use strict';
 
-}
+    var Network = function (options) {
+        var options = options || {};
+    };
 
-Network.prototype = {
-    train: function (input) {
-        //var sizes = _([inputSize, hiddenSizes, outputSize]).flatten();
-        //this.initialize(sizes);
-    },
-    run: function (input) {
-        //var sizes = _([inputSize, hiddenSizes, outputSize]).flatten();
-        //this.initialize(sizes);
+    Network.prototype = {
+        train: function (input) {
+            //var sizes = _([inputSize, hiddenSizes, outputSize]).flatten();
+            //this.initialize(sizes);
+        },
+        run: function (input) {
+            //var sizes = _([inputSize, hiddenSizes, outputSize]).flatten();
+            //this.initialize(sizes);
+        }
     }
-}
 
-var Neuron = function () {
+    global.Network = Network;
 
-}
+    var Neuron = function () {
 
-Neuron.prototype = {
-    init: function (sizes) {
-        this.mul = [];
-        this.weight = [];
-        this.input = [];
-        this.limit = 9;
     }
-}
+
+    Neuron.prototype = {
+        init: function (sizes) {
+            this.mul = [];
+            this.weight = [];
+            this.input = [];
+            this.limit = 9;
+        }
+    }
+
+})(netjs);
