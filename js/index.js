5d5e47a34cbaacc1f3a0fd25bc505d08db02e696
pyjioh <pyjioh@gmail.com>
2016-07-02 18:36:18 +0500

    Add 'clear' behavior

--- js/index.js
+++ js/index.js
@@ -1,16 +1,22 @@
 (function () {
     var app = angular.module('app', []).controller('ctrl',
         function ($scope) {
+            const black = {'background-color':'black'};
+            const white = {'background-color':'white'};
             $scope.click = function (n, m) {
-                const black = {'background-color':'black'};
-                const white = {'background-color':'white'};
                 $scope['cellStyle' + n + m] = JSON.stringify($scope['cellStyle' + n + m]) == (JSON.stringify(black)) ? white : black;
-            }
+            };
 
             $scope.analyze = function () {
 
+            };
+
+            $scope.clear = function () {
+                for (var n = 0; n < 5; n++)
+                    for (var m = 0; m < 3; m++)
+                        $scope['cellStyle' + n + m] = white;
             }
         });
-})()
+})();
 
 
